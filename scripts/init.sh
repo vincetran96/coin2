65184f56191ba8920eb84bd0184984726a564cf1
Vince Tran <vietanhtran.96@gmail.com>
2024-06-23 23:30:17 +0700

    Work on init

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,5 +5,5 @@ THIS_SCRIPT_DIR=$(dirname "$0")
 PROJECT_ROOT=$(realpath "${THIS_SCRIPT_DIR}/..")
 HEARTBEAT_LOGDIR=${PROJECT_ROOT}/logs/app/heartbeat.log
 
-# python -m app.fetch.admin
+python -m app.fetch.admin
 python -m app.heartbeat
