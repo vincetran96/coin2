2b045b7c63289a0b3cefc6df4964bf8d1d42dcbd
Vince Tran <vietanhtran.96@gmail.com>
2024-06-30 20:20:52 +0700

    Remove redundant script

--- scripts/init.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-set -eo pipefail
-
-THIS_SCRIPT_DIR=$(dirname "$0")
-PROJECT_ROOT=$(realpath "${THIS_SCRIPT_DIR}/..")
-HEARTBEAT_LOGDIR=${PROJECT_ROOT}/logs/app/heartbeat.log
-
-python -m app.fetch.admin
-python -m app.heartbeat
