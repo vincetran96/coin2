e701946e354c3d00adaaedb429015d94c27f31a7
Vince Tran <vietanhtran.96@gmail.com>
2024-06-30 11:48:55 +0700

    Remove removal of topic in shutdown.sh

--- scripts/k8s-shutdown.sh
+++ scripts/k8s-shutdown.sh
@@ -4,9 +4,3 @@ kubectl delete \
     -f k8s/coin2-fetch-binance.yaml \
     -f k8s/coin2-insert-binance.yaml \
     -f k8s/coin2-pvc.yaml
-
-docker run -it --rm --network=host bitnami/kafka:3.6.2 \
-    kafka-topics.sh \
-    --bootstrap-server localhost:9094 \
-    --delete \
-    --topic ws-binance
