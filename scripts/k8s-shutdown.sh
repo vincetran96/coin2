d513662f5bf5b9a43763fb8ba57c99bc4f1b46ea
Vince Tran <vietanhtran.96@gmail.com>
2024-07-06 12:20:35 +0700

    Rename k8s yaml files

--- scripts/k8s-shutdown.sh
+++ scripts/k8s-shutdown.sh
@@ -2,6 +2,6 @@
 
 kubectl delete \
     -f k8s/coin2-heartbeat.yaml \
-    -f k8s/coin2-fetch-binance.yaml \
-    -f k8s/coin2-insert-binance.yaml \
+    -f k8s/coin2-fetch.yaml \
+    -f k8s/coin2-insert.yaml \
     -f k8s/coin2-pvc.yaml
--- scripts/k8s-start.sh
+++ scripts/k8s-start.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 
 kubectl apply \
     -f k8s/coin2-heartbeat.yaml \
-    -f k8s/coin2-fetch-binance.yaml \
-    -f k8s/coin2-insert-binance.yaml \
+    -f k8s/coin2-fetch.yaml \
+    -f k8s/coin2-insert.yaml \
     -f k8s/coin2-pvc.yaml
