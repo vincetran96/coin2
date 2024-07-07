fe27e25614b67a57d4dfd9bf09854d81f7e095f5
Vince Tran <vietanhtran.96@gmail.com>
2024-07-07 20:54:10 +0700

    Refactor binance

--- app/fetch/binance.py
+++ app/fetch/binance.py
@@ -1,12 +1,17 @@
 """Fetch data from Binance
 
-Rate limit: https://binance-docs.github.io/apidocs/spot/en/#limits
+Rate limit:
+  - https://binance-docs.github.io/apidocs/spot/en/#limits
+  - https://developers.binance.com/docs/binance-spot-api-docs/web-socket-streams#websocket-limits
 """
 # pylint: disable-all
 # noqa: E501
 import asyncio
 import json
 import logging
+import math
+import time
+from concurrent.futures import ThreadPoolExecutor
 from random import random
 from typing import List, NoReturn
 
@@ -21,10 +26,11 @@ from app.fetch.kafka import send_to_kafka
 
 
 HTTP_URI = "https://api.binance.com/api/v3"
-WS_URI = "wss://stream.binance.com:9443/ws"
+WS_URI = "wss://stream.binance.com:443/ws"
 KAFKA_TOPIC = "ws-binance"  # Use OS env var
 BACKOFF_TIME = 3.0
-MAX_SYMBOLS_PER_CONNECTION = 100  # Manually adjusted
+SLEEP_BETWEEN_CONNECTIONS = 0.6  # Every 5 mins is limited to 500 connections
+MAX_SYMBOLS_PER_CONNECTION = 200  # Manually adjusted
 
 
 def get_symbols(limit: int = 1000) -> List[str]:
@@ -45,6 +51,9 @@ def get_symbols(limit: int = 1000) -> List[str]:
 
 async def _subscribe(symbols: List[str], con_id: int = 0) -> NoReturn:
     """Subscribe to symbols candle lines data, 1m interval
+    Source:
+      - https://binance-docs.github.io/apidocs/spot/en/#kline-candlestick-streams-for-utc
+      - https://developers.binance.com/docs/binance-spot-api-docs/web-socket-streams#klinecandlestick-streams-for-utc
 
     For each message received,
       - Check for the confirmation of whether our subscribe message is successful
@@ -64,7 +73,7 @@ async def _subscribe(symbols: List[str], con_id: int = 0) -> NoReturn:
                     "method": "SUBSCRIBE",
                     "id": con_id,
                     "params": [
-                        f"{symbol.lower()}@kline_1m"
+                        f"{symbol.lower()}@kline_1s"
                         for symbol in symbols
                     ]
                 })
@@ -79,7 +88,7 @@ async def _subscribe(symbols: List[str], con_id: int = 0) -> NoReturn:
                     ("result" in msg and msg["result"])
                     or ("error" in msg)
                 ):
-                    raise ValueError("Something wrong with our subscribe msg")
+                    raise ValueError(f"Something wrong with our subscribe msg:\n{msg}")
                 elif "s" not in msg:
                     pass
                 else:
@@ -108,23 +117,29 @@ async def _subscribe(symbols: List[str], con_id: int = 0) -> NoReturn:
             backoff_delay *= (1 + random())
 
 
-async def subscribe_symbols(symbols: List, batchsize: int):
-    """Subscribe to symbols in batch
+def run_subscribe(symbols: List[str], con_id: int):
+    """Run a single subscribe connection
 
-    For every `batchsize` symbols, create a new connection and
-    subscribe to those symbols
+    i.e., subscribe to `symbols` with a new connection
 
     """
-    await asyncio.gather(*(
-        _subscribe(symbols=symbols[i:i + batchsize], con_id=int(i / batchsize))
-        for i in range(0, len(symbols), batchsize)
-    ))
+    asyncio.run(_subscribe(symbols=symbols, con_id=con_id))
 
 
 if __name__ == "__main__":
     logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
-    asyncio.run(
-        subscribe_symbols(
-            symbols=get_symbols(limit=None), batchsize=MAX_SYMBOLS_PER_CONNECTION
-        )
-    )
+    symbols = get_symbols(limit=None)
+    batchsize = MAX_SYMBOLS_PER_CONNECTION
+    max_workers = math.ceil(len(symbols) / batchsize)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for i in range(0, len(symbols), batchsize):
+            executor.submit(
+                run_subscribe,
+                symbols=symbols[i:i + batchsize],
+                con_id=int(i / batchsize)
+            )
+            logging.info(f"Sleeping for {SLEEP_BETWEEN_CONNECTIONS:.2f}s")  # Delay submitting futures
+            time.sleep(SLEEP_BETWEEN_CONNECTIONS)
+        for future in futures:
+            future.result()
